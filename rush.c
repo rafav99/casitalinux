c30c6c5a08b1c78a2e23d12165cef9ecc9a4910d
rafa <rafav.99@hotmail.com>
2024-06-02 20:11:44 +0200

    all

--- rush.c
+++ rush.c
@@ -2,188 +2,333 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void swap(char *x, char *y) {
-    char temp = *x;
+void swap(char *x, char *y)
+{
+    char temp;
+
+    temp = *x;
     *x = *y;
     *y = temp;
 }
 
-void fill_vdata(char **row, char *argv[])
+void fill_vdata(char **mtx, char *argv[], int n)
 {
   int i;
   int a;
   i = 1;
   a = 0;
-  while(i < 5)
+  while(i < n + 1)
   {
     if (a % 2 == 0)
     {
-      row[0][i] = argv[1][a];
-      row[5][i] = argv[1][a + 8];
+      mtx[0][i] = argv[1][a];
+      mtx[n + 1][i] = argv[1][a + (2 * n)];
       i++;
     }
     a++;
   }
 }
-
-void fill_hdata(char **row, char *argv[])
+void fill_hdata(char **mtx, char *argv[], int n)
 {
   int i;
   int a;
   i = 1;
   a = 0;
-  while(i < 5)
+  while(i < n + 1)
   {
     if (a % 2 == 0)
     {
-      row[i][0] = argv[1][a + 16];
-      row[i][5] = argv[1][a + 24];
+      mtx[i][0] = argv[1][a + (4 * n)];
+      mtx[i][n + 1] = argv[1][a + (6 * n)];
       i++;
     }
     a++;
   }
 }
-
-void mat_start(char **row) {
+void mat_start(char **mtx, int n) 
+{
   int i;
   int a;
+
   i = 0;
-  while(i < 6)
+  while(i < n + 2)
   {
-    row[i] = malloc(6);
+    mtx[i] = malloc(n + 2);
     a = 0;
-    while(a < 6)
+    while(a < n + 2)
     {
-      row[i][a] = '0' + a;
+      mtx[i][a] = '0' + a;
       a++;
     }
     i++;
   }
+  mtx[0][0] = 'n';
 }
 
-void write_mat(char **row)
+void write_mat(char **mtx, int n)
 {
   int i;
-  int a;
-
-  i = 0;
-  while(i < 6)
+  int a;  
+  
+  i = 1;
+  while(i < n + 1)
   {
-    a = 0;
-    while (a < 6)
+    a = 1;
+    while (a < n + 1)
     {
-      write(1, &row[i][a], 1);
+      write(1, &mtx[i][a], 1);
       a++;
+      if (a != n + 1)
+        write(1, " ", 1);
     }
     write(1, "\n", 1);
     i++;
   }
 }
 
-int rev_checkcomb(char **row, int nrow)
+int rccomb(char **mtx, int nrow, int n)
 {
   int i;
   int maxheight;
   int count;
+
   count = 0;
   maxheight= '0';
-  i = 4;
+  i = n;
   while(i >= 1)
   {
-    if(row[nrow][i] > maxheight)
+    if(mtx[nrow][i] > maxheight)
     {
-      maxheight = row[nrow][i];
+      maxheight = mtx[nrow][i];
       count++;
     }
       i--;
   }
-  if(row[nrow][5] - '0'== count)
+  if(mtx[nrow][n + 1] - '0'== count)
     return (1);
   return(0);
 }
 
-int check_vcomb(char **row, int rpos)
-{
- 
- 
-  return(1);
-
-}
-
-int check_comb(char **row, int nrow)
+int c_comb(char **mtx, int nrow, int n)
 {
   int i;
   int maxheight;
-  int count;
-
+  int count;  
+  
   count = 0;
   maxheight= '0';
   i = 1;
-  while(i < 5)
+  while(i < n + 1)
   {
-    if(row[nrow][i] > maxheight)
+    if(mtx[nrow][i] > maxheight)
     {
-      maxheight = row[1][i];
+      maxheight = mtx[nrow][i];
       count++;
     }
       i++;
   }
-  if(row[nrow][0] - '0'== count &&  rev_checkcomb(row, nrow) == 1)
+  if(mtx[nrow][0] - '0'== count && rccomb(mtx, nrow, n) == 1)
     return (1);
   return(0);
 }
 
-void row_comb(char **row, int start, int n, int nrow)
+int c_peat(char **mtx, int nrow, int n)
 {
   int i;
-  int a;
-  if(start == n)
+  int a;  
+  a = 1;
+  while (a < n + 1)
   {
-    if(check_comb(row, nrow) == 1 )
-    {
-      a = nrow + '0';
-      write(1, "nrow es ", 8);
-      write(1, &a, 1);
-            write(1, "\n", 1); 
-
-      write_mat(row);
-      write(1, "\n", 1); 
-      if (nrow < 2)
+      i = 1;
+      while (i < nrow)
       {
-        row_comb(row, 1, 4, nrow + 1);
+        if (mtx[nrow][a] == mtx[i][a])
+          return (0);
+        i++;
       }
-      
-    }
-   
-    
-    return;
+      a++;
   }
-  i = start;
-  while(i <= n)
-  {
-    swap(&row[nrow][start], &row[nrow][i]);
-    row_comb(row, start + 1, n, nrow);
-    swap(&row[nrow][start], &row[nrow][i]);
-    i++;
-  }
+  return(1);
+}
 
+void vertical(char **mtx, int n, int *allcount, int a)
+{
+    int i;
+    int maxheight;
+    int count;
+
+    i = 1;
+    count = 0;
+    maxheight = '0';
+    while (i <= n)
+    {
+        if(mtx[i][a] > maxheight)
+        {
+            maxheight = mtx[i][a];
+            count++;
+        }
+        i++;
+    }
+    if(mtx[0][a] - '0'== count)
+        *allcount = *allcount + 1;
+}
+
+void rvertical(char **mtx, int n, int *revcount, int a)
+{
+    int i;
+    int maxheight;
+    int count;
+
+    i = n;
+    count = 0;
+    maxheight = '0';
+    while (i >= 0)
+    {
+        if(mtx[i][a] > maxheight)
+        {
+            maxheight = mtx[i][a];
+            count++;
+        }
+        i--;
+    }
+    if(mtx[n + 1][a] - '0' == count)
+        *revcount = *revcount + 1;
+}
+
+int cvert(char **mtx, int n)
+{
+    int a;
+    int allcount;
+    int revcount;
+
+    revcount = 0;
+    a = 1;
+    allcount = 0;
+    while (a <= n)
+    {
+        vertical(mtx, n, &allcount, a);
+        rvertical(mtx, n, &revcount, a);
+        a++; 
+    }
+    if (allcount == n && revcount == n)
+            return(1);
+    else
+        return(0);
+}
+
+void mtx_comb(char **mtx, int start, int n, int nrow)
+{
+    int i;
+    
+    if(start == n)
+    {
+        if(c_comb(mtx, nrow, n) == 1  && c_peat(mtx, nrow, n) == 1)
+        {
+            if (nrow < n + 1)
+                mtx_comb(mtx, 1, n, nrow + 1);
+            if (nrow == n  && cvert(mtx, n) == 1 && mtx[0][0] != 'f')
+            {
+                mtx[0][0] = 'f';
+                write_mat(mtx, n);
+            }
+        }
+        return;
+    }
+    i = start;
+    while(i <= n)
+    {
+        swap(&mtx[nrow][start], &mtx[nrow][i]);
+        mtx_comb(mtx, start + 1, n, nrow);
+        swap(&mtx[nrow][start], &mtx[nrow][i]);
+        i++;
+    }
+}
+
+int ismol(int count, char *str)
+{
+    int i;
+    int n;
+
+    n = count / 4 + '0';
+    i = 0;
+    while(str[i] != '\0')
+    {
+        if(i % 2 == 0)
+        {
+            if (str[i] > n)
+                return (0);
+        }
+        i++;
+    }
+    if (count >= 16 && count % 4 == 0 && str[i - 1] != ' ')
+        return (1);
+    else
+        return (0);
+}
+
+int check_input(char *str)
+{
+    int i;
+    int count;
+
+    count = 0;
+    i = 0;
+    while(str[i] != '\0')
+    {
+        if (i % 2 == 0)
+        {
+            if (str[i] < '0' || str[i] > '9' )
+                return (-1);
+            count ++;
+        }
+        if  (i % 2 != 0)
+        {
+            if (str[i] != ' ')
+                return (0);
+        }
+        i++;
+    }
+    if (ismol(count, str) != 0)
+        return (count / 4);
+    else
+        return (0);
+}
+
+void    freemem(char **mtx, int n)
+{
+    int i;
+
+    i = 0;
+    while(i < n + 2)
+    {
+        free(mtx[i]);
+        i++;
+    }
+    free(mtx);
 }
 int  main(int argc, char *argv[])
-{
-  char **row;
+{   
+    char **mtx; 
+    int     n;
 
-  row = malloc(6*sizeof(*row));
-
-  mat_start(row);
-  fill_vdata(row, argv);
-  fill_hdata(row, argv);
-
-  write_mat(row);
-  write(1, "\n\n\n", 3);
-
-  row_comb(row, 1, 4, 1);
-
-
-  /* code */
-  return 0;
+    if (argc != 2)
+    {
+        write(1, "Error\n", 6);
+        return (0);
+    }
+    n = check_input(argv[1]);
+    if(n != 0)
+    {
+        mtx = malloc((n + 2)*sizeof(*mtx));
+        mat_start(mtx, n);
+        fill_vdata(mtx, argv, n);
+        fill_hdata(mtx, argv, n);
+        mtx_comb(mtx, 1, n, 1);
+        if (mtx[0][0] == 'n')
+            write(1, "Error\n", 6);
+        freemem(mtx, n);
+    }
+    else
+        write(1, "Error\n", 6);
+    return 0;
 }
